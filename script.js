154dc9ea9dfecc1c3a4afb7420323ad3c1bfceac
DenisSagitov <d.sagitov@aventica.ru>
2024-10-03 14:01:48 +0300

    task example

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 fetch("data.json")
   .then((response) => response.json())
   .then((data) => {
+    const tableHeader = document.getElementById('tableHeader')
     const tableBody = document.getElementById("tableBody");
     const sortSalaryButtonMax = document.getElementById("sortSalaryButtonMax");
     const sortSalaryButtonMin = document.getElementById("sortSalaryButtonMin");
@@ -11,22 +12,83 @@ fetch("data.json")
 
     function createArray(obj) {
 
-      let flatArray = [];
+      // let flatArray = [];
+      //
+      // for (let key in obj) {
+      //
+      //   let keyIndex = obj[key];
+      //
+      //   if (Array.isArray(keyIndex)) {
+      //     flatArray = flatArray.concat(createArray(keyIndex));
+      //   } else {
+      //     flatArray.push(keyIndex);
+      //   }
+      // };
+      // return flatArray;
 
+      /**
+       * [DENIS]
+       * Немного переделал.
+       * В дате значением obj[key] всегда является массив, нет смысла его чекать
+       * */
+      let flatArray = []
       for (let key in obj) {
-        
-        let keyIndex = obj[key];
-
-        if (Array.isArray(keyIndex)) {
-          flatArray = flatArray.concat(createArray(keyIndex));
-        } else {
-          flatArray.push(keyIndex);
+        let someRootEmployees = obj[key];
+        for (let rootEmployee of someRootEmployees) {
+          flatArray.push(
+            ...unwrapEmployeeIntoArray(rootEmployee)
+          )
         }
-      };
-      return flatArray;
+      }
+      return flatArray
     };
 
+    /**
+     * [DENIS]
+     * Типы просто для удобства
+     * */
+
+    /** @typedef {{
+     * id: string;
+     * name: string;
+     * role: [ string, string ];
+     * workload: string;
+     * salary: string;
+     * subordinates?: AnyEmployee[];
+     }} AnyEmployee */
+
+    /** @typedef { AnyEmployee & {
+     * company: [ string, string ];
+     } } RootEmployee */
+
+    /** @typedef { RootEmployee } EmployeeWithEnsuredCompany */
+
+    /**
+     * [DENIS]
+     * Рекурсивная функция
+     * На вход - сотрудник
+     * На выходе - обязательно массив сотрудников, даже если у сотрудника на входе не было подчиненных
+     *
+     * @param { EmployeeWithEnsuredCompany } employee
+     * @returns { EmployeeWithEnsuredCompany[] } */
+    function unwrapEmployeeIntoArray(employee) {
+      // Если нет подчиненных -
+      if (!employee.subordinates) {
+        return [ employee ]
+      }
+
+      const unwrappedSubordinates = []
+      for (let subordinate of employee.subordinates) {
+        const subordinateWithEnsuredCompany = { ...subordinate, company: employee.company }
+        unwrappedSubordinates.push(...unwrapEmployeeIntoArray(subordinateWithEnsuredCompany))
+      }
+      return [ employee, ...unwrappedSubordinates ]
+    }
+
     const allEmployees = createArray(data);
+
+    // [DENIS] Проверка того, что самый глубокий подчиненный анврапнулся
+    console.log('find deepest employee', allEmployees.find(it => it.id === '3369d912-9cfe-4e43-9e31-d6f510c068bc'))
 
     // Функция для создания ячеек с информацией
 
@@ -44,6 +106,25 @@ fetch("data.json")
         tableBody.appendChild(row);
       });
     };
+
+    /** [DENIS]
+     * Перерендер хедера таблицы для сотрудников и компаний
+     * */
+    /** @param { string[] } array */
+    function createTableHeader(array) {
+      tableHeader.innerHTML = ''
+      array.forEach(it => {
+        const element = document.createElement('th')
+        element.textContent = it
+        tableHeader.appendChild(element)
+      })
+    }
+    function createEmployeesHeader() {
+      createTableHeader([ 'Имя', 'Роль', 'Зарплата', 'Компания', 'Подчиненных' ])
+    }
+    function createCompaniesHeader() {
+      createTableHeader([ 'ID компании', 'Название', 'Сумма' ])
+    }
 
     // Функция для сортировки списка по зарплате (От наибольшей к наименьшей)
 
@@ -125,6 +206,7 @@ fetch("data.json")
 
     function showEmployees(employees) {
       tableBody.innerHTML = "";
+      createEmployeesHeader()
       createRows(employees);
     };
 
@@ -132,6 +214,63 @@ fetch("data.json")
 
     function resetSorting() {
       showEmployees(allEmployees);
+    };
+
+    /** @typedef {{
+     * id: string;
+     * name: string;
+     * salarySum: number;
+     }} CompanyResult */
+
+    /**
+     * [DENIS]
+     * Получить компании из даты
+     *
+     * @param { EmployeeWithEnsuredCompany[] } employees
+     * @returns { CompanyResult[] }
+    */
+    function getCompanies(employees) {
+      /** @type { Record<string, CompanyResult> } */
+      const mapByCompanyId = {}
+      employees.forEach(employee => {
+        const companyId = employee.company[0]
+        if (!mapByCompanyId[companyId]) {
+          mapByCompanyId[companyId] = {
+            id: companyId,
+            name: employee.company[1],
+            salarySum: 0,
+          }
+        }
+        mapByCompanyId[companyId].salarySum += Number(employee.salary.replace('$', ''))
+      })
+      return Object.values(mapByCompanyId)
+    }
+
+    /** [DENIS]
+     * Отсортировать компании
+     *
+     * @param { CompanyResult[] } companies
+     * @returns { CompanyResult[] } */
+    function sortBySalarySum(companies) {
+      return companies.sort((a, b) => b.salarySum - a.salarySum)
+    }
+
+    /** [DENIS]
+     * Рендер компаний
+     *
+     * @param { CompanyResult[] } companies */
+    function showCompanies(companies) {
+      createCompaniesHeader()
+      tableBody.innerHTML = "";
+      companies.forEach((company) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+          <td>${company.id}</td>
+          <td>${company.name}</td>
+          <td>${company.salarySum}</td>
+        `;
+        tableBody.appendChild(row);
+      });
     };
 
     // Кнопки
@@ -149,6 +288,17 @@ fetch("data.json")
     });
 
     resetSortButton.addEventListener("click", resetSorting);
+
+    /** [DENIS]
+     * Новая кнопка
+     * */
+    document.getElementById('getCompaniesBySalarySum').addEventListener('click', () => {
+      showCompanies(
+        sortBySalarySum(
+          getCompanies(allEmployees)
+        )
+      )
+    })
 
     showEmployees(allEmployees);
   });
